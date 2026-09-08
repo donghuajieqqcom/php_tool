b1edad7d17fc52a48fbf8219c5e27f951644e57d
donghuajieqqcom <donghuajie@qq.com>
2026-09-08 09:45:57 +0800

    Update route.php

--- route/route.php
+++ route/route.php
@@ -1,11 +1,11 @@
 <?php
 
 //后台域名
-Route::domain('192.168.1.3', 'admin');/*填写你的后台域名*/
+Route::domain('anytoolbox.cn', 'admin');/*填写你的后台域名*/
 
 //前台域名或ip
-Route::domain('192.168.1.3', function () {/*填写你的域名*/
-    Route::domain('192.168.1.3', 'index');/*填写你的域名*/
+Route::domain('anytoolbox.cn', function () {/*填写你的域名*/
+    Route::domain('anytoolbox.cn', 'index');/*填写你的域名*/
     Route::rule('404', 'index/e404');
     //接口
     Route::rule('doapi', 'index/api');
